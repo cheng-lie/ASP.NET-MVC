c3431d828bccaa16e3f4a5d34599744f386962dd
jincl <jincl@sh-db.com.cn>
2019-11-20 18:48:05 +0800

    关于ninject

---
+++ BookShop/BookShop.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace BookShop.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory() {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
